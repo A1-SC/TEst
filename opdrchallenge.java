b9c273e4ce9461472a36bd5b18d9f2f6d147a155
Goeieman71 <25147412@student.hhs.nl>
2025-11-12 14:50:25 +0100

    Add files via upload

---
+++ opdrchallenge.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void wachtwoord () {
+        Scanner scanner = new Scanner (System.in);
+
+    }
+
+    public static void main (String [] args) {
+        wachtwoord ();
+    }
+}
