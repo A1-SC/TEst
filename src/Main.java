ce21209318f044ae428ade10f353367bbe3a6559
ahmad <25066048@student.hhs.nl>
2025-11-12 13:56:57 +0100

    Z

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            System.out.println ("Hebben jullie geleerd boys?") ;
+String voorbereidt = scanner.nextLine ();
+if (voorbereidt.equals ("6")) {
+        System.out.println("Ahmad heeft een 10+ voor JAVA ");
+        System.out.println("Dayjiro heeft een 9 voor JAVA " );
+    System.out.println("Mohammed heeft een 9 voor JAVA " );
+
+    }
+else {
+    System.out.println("ONVOLNDOENE!!! UITSCHRIJVEN NUUUUU! DOMMERIKKE");
+}
+
+}
+    }
